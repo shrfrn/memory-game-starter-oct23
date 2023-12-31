860c90493fb794167ada395514acdad2d92bb607
BatelKat96 <batelkat@gmail.com>
2023-12-31 12:38:17 +0200

    Create dynamic dom

--- js/memory-game.js
+++ js/memory-game.js
@@ -1,8 +1,35 @@
 'use strict'
 
 function init() {
-    console.log('Hi')
+
+	const nums = initNums()
+	const strHtml =
+		nums.map(num => `
+            <article 
+                class="card" 
+                onclick="onCardClick(this)"> <div>${num}</div>
+            </article>`).join('')
+
+	document.querySelector('main').innerHTML = strHtml
 }
+
+function initNums() {
+	const nums = []
+
+	for (var i = 0; i < 20; i++) {
+		nums.push(i % 10)
+	}
+
+	for (var i = 0; i < nums.length; i++) {
+		const idx = getRandomInt(0, nums.length)
+
+		const temp = nums[idx]
+		nums[idx] = nums[i]
+		nums[i] = temp
+	}
+	return nums
+}
+
 
 function onCardClick(elCard) {
 	var elSelectedCards = document.querySelectorAll('.selected')
@@ -23,4 +50,10 @@ function onCardClick(elCard) {
 			elSelectedCards.forEach(elCard => elCard.classList.remove('selected'))
 		}, 2000)
 	}
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
 }
